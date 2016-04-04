974a573af80aa007b9f5ed4ef5723cd70cbe24e0
davidmars <d.marsalone@gmail.com>
2016-04-04 09:10:18 +0200

    clean project

--- BootstrapNavActive.js
+++ BootstrapNavActive.js
@@ -1,49 +1,63 @@
+/**
+ *
+ * BootstrapNavActive.js manages .active css class on Bootstrap nav items using javascript and the current url.
+ * @param {JQuery|HTMLElement} $nav the .navbar bootstrap element
+ * @returns {{refresh: Function, deactivateAll: Function, setActive: Function, destroy: Function}}
+ * @constructor
+ *
+ */
 var BootstrapNavActive=function($nav){
     "use strict";
 
+    $nav=$($nav);//just to be sure it is jquery object
+
     /**
      * Set a <a href> link (and its parent menu handler) as active.
-     * @param {jQuery} $hrefItem The a href tag to set active.
+     * @param {JQuery|HTMLElement} $hrefItem The a href tag to set active.
      */
     var setActive=function($hrefItem){
+        $hrefItem=$($hrefItem);//just to be sure it is jquery object
         $hrefItem.closest("li").addClass("active");
         $hrefItem.closest("li.dropdown").addClass("active");
     };
-
     /**
-     * Set links (and its menu handlers) as active .
-     * @param {string} url The url set active.
+     * deactivateAll all .active elements in $na
      */
-    var setActiveAccordingUrl=function(url){
-        console.log("setActiveAccordingUrl",url);
+    var deactivateAll=function(){
+        $nav.find(".active").removeClass("active");
     };
 
     /**
-     * refresh active and inactive items according the current location url
+     * Refresh items according the current location url
      */
     var refresh=function(){
         //reset all before
-        $nav.find(".active").removeClass("active");
-        $nav.find("a").each(
+        deactivateAll();
+
+        $nav.find("li>a").each(
             function(){
                 var link=$(this)[0];
                 var loc=location;
-                if(link.href===loc.href){ //test real href and so works in all cases (file:/, relative, absolute etc...)
+                if(link.href===loc.href){ //test real href and so works in all cases (file:/, relative, absolute etc...). Using only attributes selectors didn't work for all scenarios.
                     setActive($(this));
                 }
         });
     };
 
-    //first call
+    var destroy=function(){
+        window.removeEventListener("hashchange",refresh);
+    };
+
+    //listen for page changes (for html5 url management or #anchors links)
+    window.addEventListener("hashchange",refresh);
+    //init call
     refresh();
 
-    //
-    window.addEventListener("hashchange",function(){
-        refresh();
-    });
-
     return {
-        refresh:refresh
+        refresh:refresh,
+        deactivateAll:deactivateAll,
+        setActive:setActive,
+        destroy:destroy
     }
 
 };
