9c5c168c8efffd8e97dc3d577f735395d7486f3d
davidmars <d.marsalone@gmail.com>
2016-04-05 06:25:40 +0200

    add pills & tabs examples

--- example/js/main.js
+++ example/js/main.js
@@ -2,5 +2,11 @@ $( document ).ready(
     function(){
         var $myTopNav=$("#myTopNav");
         new BootstrapNavActive($myTopNav);
+
+        var $myTabs=$("#myTabs");
+        new BootstrapNavActive($myTabs);
+
+        var $myPills=$("#myPills");
+        new BootstrapNavActive($myPills);
     }
 );
